01d33fb4ba38b39bc2ba4bba3bb38c5da8397c3c
Bryan Cora <corabj@mail.uc.edu>
2025-09-15 12:37:12 -0400

    fix smart pointers

---
+++ cpp/main.cpp
@@ -0,0 +1,18 @@
+#include "Trie.hpp"
+#include <iostream>
+
+int main() {
+    Trie<int> trie;
+    trie.insert("cat", 1);
+    trie.insert("car", 2);
+    trie.insert("dog", 3);
+
+    auto v = trie.get("car");
+    if (v) std::cout << "car = " << *v << "\n";
+
+    auto ac = trie.autocomplete("ca");
+    std::cout << "Autocomplete ca -> ";
+    for (auto &x : ac) std::cout << x << ' ';
+    std::cout << "\n";
+    return 0;
+}
