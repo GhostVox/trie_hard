2a16521e16ab248cb1462ea4f76bdaf42e228494
Bryan Cora <corabj@mail.uc.edu>
2025-09-15 11:08:39 -0400

    partial application of autocomplete

--- cpp/Trie.hpp
+++ cpp/Trie.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "TrieNode.hpp"
 #include <memory>
 #include <optional>
@@ -12,8 +13,8 @@ class Trie {
     void insert(std::string &, T &);
     std::optional<T> get(std::string &);
     bool remove(std::string);
-    std::vector<T> prefixSearch(std::string);
-    std::vector<T> autocomplete(std::string);
+    bool prefixSearch(std::string);
+    std::vector<T> autocomplete(std::string, int = 50);
     std::vector<T> suggest(std::string);
 
   private:
@@ -48,7 +49,6 @@ std::optional<T> Trie<T>::get(std::string &key) {
     return current->getValue();
 }
 
-// TODO(brinhasavlin): this method is incomplete
 template <typename T>
 bool Trie<T>::remove(std::string key) {
     std::unique_ptr<TrieNode<T>> current = root;
@@ -61,7 +61,6 @@ bool Trie<T>::remove(std::string key) {
         visited.push_back(current);
         current = current->getChild(c);
     }
-    // WARNING(brinhasavlin): not sure if this deletion is correct
     if (current == root) {
         delete root;
         root = nullptr;
@@ -78,4 +77,39 @@ bool Trie<T>::remove(std::string key) {
         last -= 1;
     }
     return true;
+}
+
+template <typename T>
+bool Trie<T>::prefixSearch(std::string key) {
+    std::unique_ptr<TrieNode<T>> current = root;
+    // std::vector<T>
+
+    for (char c : key) {
+        if (!current->getChild(c)) {
+            return false;
+        }
+        current = current->getChild(c);
+    }
+    return true;
+}
+
+template <typename T>
+std::vector<T> Trie<T>::autocomplete(std::string key, int limit) {
+    std::unique_ptr<TrieNode<T>> current = root;
+    std::vector<T> words;
+    for (char c : key) {
+        if (!current->getChild(c)) {
+            return words;
+        }
+        current = current->getChild(c);
+    }
+
+    if (current.isEnd()) {
+        words.push_back(current.getValue());
+    }
+
+    for (TrieNode<T> child : current) {
+    }
+
+    return words;
 }
